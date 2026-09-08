0068d89acc1e1ab403aff8ecb567fa7c1ca1dfaa
mirozdanie6v <49186663+mirozdanie6v@users.noreply.github.com>
2026-09-08 19:52:45 +0700

    Add React application entrypoint

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import '../styles.css';
+import { App } from './app/App';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('React root not found');
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
